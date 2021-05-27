bccd351ed9417572f440510caddc81cc20c7a552
Fat brother <regret_f@qq.com>
2021-05-27 19:57:42 +0800

    第一次提交

---
+++ objc4-824（已配置）/FNTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FNTest
+//
+//  Created by Fat brother on 2021/5/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
