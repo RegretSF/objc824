e94628d6874c2b93e0e1566a48c4b3da2c505543
Fat brother <regret_f@qq.com>
2021-11-16 17:29:17 +0800

    Objective-C运行时的改进

---
+++ 代码/02-class的原理/03-Objective-C运行时的改进/Objective-C运行时的改进/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C运行时的改进
+//
+//  Created by TT-Fangss on 2021/11/16.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
