69c851f239c525695d962f425f8e3e381207137a
Fat brother <regret_f@qq.com>
2021-12-07 14:47:21 +0800

    objc_msgSend 汇编实现

---
+++ 代码/02-class的原理/07-objc_msgSend汇编实现/objc_msgSend汇编实现/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  objc_msgSend介绍
+//
+//  Created by TT-Fangss on 2021/12/6.
+//
+
+#import <Foundation/Foundation.h>
+
+/*
+ 
+ */
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
