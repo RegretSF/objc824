9dc6271650fbb5c19c4fd276b041b8d79baf9df3
Fat brother <regret_f@qq.com>
2021-12-24 15:52:03 +0800

    动态方法解析完成流程笔记

--- 代码/03-消息传递流程/05-消息传递：动态方法解析/消息传递：动态方法解析/main.m
+++ 代码/03-消息传递流程/05-消息传递：动态方法解析/消息传递：动态方法解析/main.m
@@ -6,52 +6,82 @@
 //
 
 #import <Foundation/Foundation.h>
-@interface SHPerson : NSObject
-- (void)helloWorld;
-@end
-@implementation SHPerson
-- (void)helloWorld {
+#import <objc/runtime.h>
+
+void dynamicMethodIMP(id self, SEL _cmd) {
     NSLog(@"%s", __func__);
 }
+
+@interface SHPerson : NSObject
++ (void)helloWorld;
 @end
 
-@interface SHStudent : SHPerson
-- (void)play_1;
-+ (void)play_2;
-- (void)play_3;
+@implementation SHPerson
+//+ (BOOL)resolveClassMethod:(SEL)sel {
+//    if (sel == @selector(helloWorld)) {
+//        NSLog(@"%s", __func__);
+//        return class_addMethod(objc_getMetaClass("SHPerson"), sel, (IMP)dynamicMethodIMP, "v@:");
+//    }
+//    return [super resolveClassMethod:sel];
+//}
+
+//+ (BOOL)resolveInstanceMethod:(SEL)sel {
+//    if (sel == @selector(helloWorld)) {
+//        NSLog(@"%s", __func__);
+//        return class_addMethod([self class], sel, (IMP)dynamicMethodIMP, "v@:");
+//    }
+//    return [super resolveInstanceMethod:sel];
+//}
 @end
-@implementation SHStudent
-- (void)play_1 {
-    NSLog(@"%s", __func__);
-}
-@end
+
+//@interface SHStudent : SHPerson
+//- (void)play_1;
+//+ (void)play_2;
+//- (void)play_3;
+//@end
+//@implementation SHStudent
+//- (void)play_1 {
+//    NSLog(@"%s", __func__);
+//}
+//@end
 
 @implementation NSObject(Category)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
+//+ (BOOL)resolveInstanceMethod:(SEL)sel {
+//    if (sel == NSSelectorFromString(@"play_3")) {
+//        NSLog(@"%s",__func__);
+//    }
+//
+//    return NO;
+//}
 + (BOOL)resolveInstanceMethod:(SEL)sel {
-    if (sel == NSSelectorFromString(@"play_3")) {
-        NSLog(@"%s",__func__);
+    if (sel == @selector(helloWorld)) {
+        NSLog(@"%s", __func__);
+        return class_addMethod([self class], sel, (IMP)dynamicMethodIMP, "v@:");
     }
-    
     return NO;
 }
 
 
-+ (BOOL)resolveClassMethod:(SEL)sel {
-    NSLog(@"%s",__func__);
-    return NO;
-}
+//+ (BOOL)resolveClassMethod:(SEL)sel {
+//    NSLog(@"%s",__func__);
+//    return NO;
+//}
 #pragma clang diagnostic pop
 @end
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        SHStudent *s = [[SHStudent alloc] init];
+        
+//        SHPerson *p = [[SHPerson alloc] init];
+        [SHPerson helloWorld];
+        
+//        SHStudent *s = [[SHStudent alloc] init];
 //        [s helloWorld];
 //        [s play_1];
-        [SHStudent play_2];
-        [s play_3];
+//        [SHStudent play_2];
+//        [s play_3];
 
         
     }
@@ -96,7 +126,7 @@ int main(int argc, const char * argv[]) {
  behavior = 3，LOOKUP_RESOLVER = 2，3 & 2 = 2。条件成立，所以进入 if 的代码块。
  ^= : 异或运算符，相同为0，不同为 1。3 ^= 2 = 1，那么 behavior = 1，传入 resolveMethod_locked 函数。
  
- ### 一、resolveMethod_locked 函数
+ ### 1、resolveMethod_locked 函数
  这是 resolveMethod_locked 函数的内部实现：
  ```swift
  static NEVER_INLINE IMP
@@ -139,7 +169,7 @@ int main(int argc, const char * argv[]) {
  resolveMethod_locked 函数的返回值是一个 IMP。前面一些锁的处理我们先不看。注意看第一个判断，if (! cls->isMetaClass()) 是判断函数传进来的 cls 是否是一个元类对象。
  我们知道，在类的结构中，存在着类对象和元类对象，它们的结构是一样的，都是 Class。区别就在于，类对象存储的实例方法，而元类对象存储类方法。所以这个判断的本质上是判断要动态解析实例方法还是类方法。
  
- ### 二、动态解析实例方法
+ ### 2、动态解析实例方法
  先来看第一个动态解析实例方法 - resolveInstanceMethod 函数的实现：
  ```swift
  static void resolveInstanceMethod(id inst, SEL sel, Class cls)
@@ -186,7 +216,7 @@ int main(int argc, const char * argv[]) {
  
  resolveInstanceMethod 方法的返回值是 BOOL 类型，通过这个返回值告诉系统，我们是否动态方法解析了。不管我们有没有解析，系统都会把 sel 缓存下来，避免重复触发 resolveInstanceMethod 方法。
  
- ### 三、动态解析类方法
+ ### 3、动态解析类方法
  接下来是动态解析类方法，过程和解析实例方法差不多，解析类方法的要实现 resolveClassMethod 方法，这个是解析类方法的解析器。我们来看一下 resolveClassMethod 函数的实现。
  ```swift
  static void resolveClassMethod(id inst, SEL sel, Class cls)
@@ -258,7 +288,7 @@ int main(int argc, const char * argv[]) {
  
  在整个 OC 的底层，没有所谓实例方法和类方法，这些所谓的实例方法和类方法在底层都是函数。那苹果这么做的意义是为了更加体现面向对象。在没有找到对应的类方法实现时，会去实例方法里找，所以才会动态解析实例方法。这里其实就和前面第六篇文章-Objective-C 对象的结构分析中的 isa 流程相呼应。
  
- ### 四、没有动态方法解析的处理
+ ### 4、没有动态方法解析的处理
  如果开发者没有实现动态方法解析的处理，会调用 lookUpImpOrForwardTryCache 函数，它内部的实现就是调用了 _lookUpImpTryCache 函数。
  _lookUpImpTryCache 函数的实现如下：
  ```swift
@@ -293,7 +323,125 @@ int main(int argc, const char * argv[]) {
  先判断类是否初始化，如果没有就会去之前的慢速查找的操作。
  如果类初始化了，先进行快速查找，如果缓存有，跳转至 done，否则会根据架构的不同，进行处理。
  
- ## 二、动态方法解析 OC 层面的处理
+ ## 二、动态方法解析 OC 层面处理
  
+ 在 OC 层面怎么处理呢，系统提供给开发者两个方法 resolveInstanceMethod:，resolveClassMethod:，分别对应动态实例方法解析和动态类方法解析。
+ ### 1、动态实例方法解析
+ 我们以动态实例方法解析为例，定义一个 dynamicMethodIMP 函数。
+ ```swift
+ void dynamicMethodIMP(id self, SEL _cmd) {
+     NSLog(@"%s", __func__);
+ }
+ ```
+ 
+ 定义一个 SHPerson，代码实现如下
+ ```swift
+ @interface SHPerson : NSObject
+ - (void)helloWorld;
+ @end
+ 
+ @implementation SHPerson
+ + (BOOL)resolveInstanceMethod:(SEL)sel {
+     if (sel == @selector(helloWorld)) {
+         NSLog(@"%s", __func__);
+         return class_addMethod([self class], sel, (IMP)dynamicMethodIMP, "v@:");
+     }
+     return [super resolveInstanceMethod:sel];
+ }
+ @end
+ ```
+ 
+ 在 SHPerson 中声明 helloWorld 方法，但并未实现该方法。
+ 
+ 实现动态实例方法解析器 - resolveInstanceMethod:，当 helloWorld 方法不实现的话必然会走到 resolveInstanceMethod: 方法。如果没有实现 helloWorld 方法，我们动态的将 dynamicMethodIMP 函数添加到 SHPerson，否则就 super 一下。
+ 
+ 这里需要注意的是需要导入运行时的库：#import <objc/runtime.h>
+ 
+ 调用 helloWorld 方法，并将结果打印。
+ ```swift
+ int main(int argc, const char * argv[]) {
+     @autoreleasepool {
+         SHPerson *p = [[SHPerson alloc] init];
+         [p helloWorld];
+     }
+     return 0;
+ }
+ ```
+ /Users/tt-fangss/Fangss/TmpeCode/objc824/代码/03-消息传递流程/05-消息传递：动态方法解析/消息传递：动态方法解析/动态实例方法解析的实现打印.png
+ 
+控制台成功打印出 [SHPerson resolveInstanceMethod:] 和 dynamicMethodIMP。
+ 
+ ### 2、动态类方法解析
+ 其实动态类方法解析的处理和动态实例方法解析的处理是一样的，只是解析器不一样。动态类方法解析需要实现 resolveClassMethod: 方法，还是用 dynamicMethodIMP 函数处理，SHPerson 的处理如下：
+ ```swift
+ @interface SHPerson : NSObject
+ + (void)helloWorld;
+ @end
+
+ @implementation SHPerson
+ + (BOOL)resolveClassMethod:(SEL)sel {
+     if (sel == @selector(helloWorld)) {
+         NSLog(@"%s", __func__);
+         return class_addMethod(objc_getMetaClass("SHPerson"), sel, (IMP)dynamicMethodIMP, "v@:");
+     }
+     return [super resolveClassMethod:sel];
+ }
+ @end
+ ```
+ 
+ 实现 resolveClassMethod: 方法，因为类方法是存储在元类里面，所以在调用 class_addMethod 传的第一个参数需要传 SHPerson 的元类。
+ 
+ 来看一下调用和打印的结果：
+ ```swift
+ int main(int argc, const char * argv[]) {
+     @autoreleasepool {
+         [SHPerson helloWorld];
+     }
+     return 0;
+ }
+ ```
+ /Users/tt-fangss/Fangss/TmpeCode/objc824/代码/03-消息传递流程/05-消息传递：动态方法解析/消息传递：动态方法解析/动态类方法解析的实现打印.png
+ 控制台成功打印出 [SHPerson resolveClassMethod:] 和 dynamicMethodIMP。
+ 
+ ### 3、验证源码中动态类方法解析流程
+ 那么，前面讲过，如果动态类方法解析没有实现的话会进行动态实例方法解析，下面对此进行验证。我们调用类方法 helloWorld，但是实现的是 resolveInstanceMethod: 方法。
+ 
+ 需要注意的是，底层在查找方法的时候，找到根元类了，并且没有找到方法的实现，才会去根类里面去找方法的实现。那么动态方法解析的原理也是一样，只有找到 NSObject 的元类并且没有对类方法解析器做处理，才会走实例方法解析器。
+ 
+ 代码如下：
+ ```swift
+ @interface SHPerson : NSObject
+ + (void)helloWorld;
+ @end
+
+ @implementation SHPerson
+ @end
+ 
+ @implementation NSObject(Category)
+ #pragma clang diagnostic push
+ #pragma clang diagnostic ignored "-Wobjc-protocol-method-implementation"
+ + (BOOL)resolveInstanceMethod:(SEL)sel {
+     if (sel == @selector(helloWorld)) {
+         NSLog(@"%s", __func__);
+         return class_addMethod([self class], sel, (IMP)dynamicMethodIMP, "v@:");
+     }
+     return NO;
+ }
+ #pragma clang diagnostic pop
+ @end
+ ```
+ 
+ 来看一下调用和打印的结果：
+ ```swift
+ int main(int argc, const char * argv[]) {
+     @autoreleasepool {
+         [SHPerson helloWorld];
+     }
+     return 0;
+ }
+ ```
+ /Users/tt-fangss/Fangss/TmpeCode/objc824/代码/03-消息传递流程/05-消息传递：动态方法解析/消息传递：动态方法解析/调用类方法实现实例方法解析器.png
+ 
+ 通过打印结果可以知道，虽然调用的是类方法，但是只要在 NSObject 的分类中实现 resolveInstanceMethod: 方法，一样可以做处理，这里就印证前面的源码分析是对的。到这里，整个动态方法解析的流程分析就结束了。
  
  */
