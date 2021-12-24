754d29c02c0f01e354ca1428cd35dcae5d9532f7
Fat brother <regret_f@qq.com>
2021-12-24 13:39:59 +0800

    动态方法解析的源码分析

--- 代码/03-消息传递流程/05-消息传递：动态方法解析/消息传递：动态方法解析/main.m
+++ 代码/03-消息传递流程/05-消息传递：动态方法解析/消息传递：动态方法解析/main.m
@@ -63,6 +63,7 @@ int main(int argc, const char * argv[]) {
  动态方法解析
  
  lookUpImpOrForward 函数为慢速查找流程的入口，进入慢速查找流程后，仍未找方法的实现，会进入下一个流程-动态方法解析。
+ ## 一、动态方法解析的源码分析
  我们来看一下 lookUpImpOrForward 函数调整至动态方法解析的代码：
  ```swift
  // No implementation found. Try method resolver once.
@@ -95,7 +96,7 @@ int main(int argc, const char * argv[]) {
  behavior = 3，LOOKUP_RESOLVER = 2，3 & 2 = 2。条件成立，所以进入 if 的代码块。
  ^= : 异或运算符，相同为0，不同为 1。3 ^= 2 = 1，那么 behavior = 1，传入 resolveMethod_locked 函数。
  
- ## 一、resolveMethod_locked 函数
+ ### 一、resolveMethod_locked 函数
  这是 resolveMethod_locked 函数的内部实现：
  ```swift
  static NEVER_INLINE IMP
@@ -138,7 +139,7 @@ int main(int argc, const char * argv[]) {
  resolveMethod_locked 函数的返回值是一个 IMP。前面一些锁的处理我们先不看。注意看第一个判断，if (! cls->isMetaClass()) 是判断函数传进来的 cls 是否是一个元类对象。
  我们知道，在类的结构中，存在着类对象和元类对象，它们的结构是一样的，都是 Class。区别就在于，类对象存储的实例方法，而元类对象存储类方法。所以这个判断的本质上是判断要动态解析实例方法还是类方法。
  
- ## 二、动态解析实例方法
+ ### 二、动态解析实例方法
  先来看第一个动态解析实例方法 - resolveInstanceMethod 函数的实现：
  ```swift
  static void resolveInstanceMethod(id inst, SEL sel, Class cls)
@@ -185,7 +186,7 @@ int main(int argc, const char * argv[]) {
  
  resolveInstanceMethod 方法的返回值是 BOOL 类型，通过这个返回值告诉系统，我们是否动态方法解析了。不管我们有没有解析，系统都会把 sel 缓存下来，避免重复触发 resolveInstanceMethod 方法。
  
- ## 三、动态解析类方法
+ ### 三、动态解析类方法
  接下来是动态解析类方法，过程和解析实例方法差不多，解析类方法的要实现 resolveClassMethod 方法，这个是解析类方法的解析器。我们来看一下 resolveClassMethod 函数的实现。
  ```swift
  static void resolveClassMethod(id inst, SEL sel, Class cls)
@@ -236,5 +237,63 @@ int main(int argc, const char * argv[]) {
      }
  }
  ```
+ 
+ 解析类方法的实现逻辑和解析实例方法的实现逻辑差不多，需要注意的不是 resolveClassMethod 函数的实现，而是 resolveMethod_locked 函数。来看一下 resolveMethod_locked 函数解析类方法的流程：
+ ```swift
+ // try [nonMetaClass resolveClassMethod:sel]
+ // and [cls resolveInstanceMethod:sel]
+ // 动态解析类方法。本质上是查找元类的对象方法。
+ resolveClassMethod(inst, sel, cls);
+ 
+ // lookUpImpOrNilTryCache 检查是否有缓存，目的是检查是否动态解析类方法
+ if (!lookUpImpOrNilTryCache(inst, sel, cls)) {
+     // 在动态解析类方法后，开发者在 NSObject 的处理实现的可能不是类方法，而是元类方法。
+     // 所以在这里又会对实例方法进行解析，这里就是和 isa 流程图中的 superclass 的走位相呼应。
+     // 根元类的 superclass 指针指向根类。
+     resolveInstanceMethod(inst, sel, cls);
+ }
+ ```
+ 
+ 我们可以看到，在尝试去动态的解析类方法后，会去检查缓存里是否有方法，如果没有方法，会去解析实例方法。为什么呢？
+ 
+ 在整个 OC 的底层，没有所谓实例方法和类方法，这些所谓的实例方法和类方法在底层都是函数。那苹果这么做的意义是为了更加体现面向对象。在没有找到对应的类方法实现时，会去实例方法里找，所以才会动态解析实例方法。这里其实就和前面第六篇文章-Objective-C 对象的结构分析中的 isa 流程相呼应。
+ 
+ ### 四、没有动态方法解析的处理
+ 如果开发者没有实现动态方法解析的处理，会调用 lookUpImpOrForwardTryCache 函数，它内部的实现就是调用了 _lookUpImpTryCache 函数。
+ _lookUpImpTryCache 函数的实现如下：
+ ```swift
+ static IMP _lookUpImpTryCache(id inst, SEL sel, Class cls, int behavior)
+ {
+     runtimeLock.assertUnlocked();
+
+     if (slowpath(!cls->isInitialized())) {
+         // see comment in lookUpImpOrForward
+         return lookUpImpOrForward(inst, sel, cls, behavior);
+     }
+
+     IMP imp = cache_getImp(cls, sel);
+     if (imp != NULL) goto done;
+ #if CONFIG_USE_PREOPT_CACHES
+     if (fastpath(cls->cache.isConstantOptimizedCache(true))) {
+         imp = cache_getImp(cls->cache.preoptFallbackClass(), sel);
+     }
+ #endif
+     if (slowpath(imp == NULL)) {
+         return lookUpImpOrForward(inst, sel, cls, behavior);
+     }
+     
+ done:
+     if ((behavior & LOOKUP_NIL) && imp == (IMP)_objc_msgForward_impcache) {
+         return nil;
+     }
+     return imp;
+ }
+ ```
+ 
+ 先判断类是否初始化，如果没有就会去之前的慢速查找的操作。
+ 如果类初始化了，先进行快速查找，如果缓存有，跳转至 done，否则会根据架构的不同，进行处理。
+ 
+ ## 二、动态方法解析 OC 层面的处理
+ 
  
  */
