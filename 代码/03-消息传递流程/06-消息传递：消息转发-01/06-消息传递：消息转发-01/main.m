fecef006cbdae95a37c498a0bd311633491c3f24
Fat brother <regret_f@qq.com>
2021-12-28 00:13:27 +0800

    慢速转发流程收尾

--- 代码/03-消息传递流程/06-消息传递：消息转发-01/06-消息传递：消息转发-01/main.m
+++ 代码/03-消息传递流程/06-消息传递：消息转发-01/06-消息传递：消息转发-01/main.m
@@ -173,6 +173,7 @@
  ### 2. 慢速转发流程
  当我们在 forwardingTargetForSelector: 方法做处理的时候，总会觉得奇奇怪怪的。如果 SHAnimal 也不实现 run 方法，程序一样会崩溃，毕竟只是备胎😂，所以我们不想在 forwardingTargetForSelector: 中做处理，那么就开始进入到下一个流程，叫慢速转发流程，也就是实现 methodSignatureForSelector: 方法，在 methodSignatureForSelector: 方法中做转发的处理。
  
+ #### 1. methodSignatureForSelector:
  methodSignatureForSelector: 方法需要返回一个 NSMethodSignature 对象，也就是方法签名。需要注意的是，methodSignatureForSelector: 和 forwardingTargetForSelector: 不能同时存在哦，否则就只走到 forwardingTargetForSelector: ，不会走到 methodSignatureForSelector: 。
  
  代码如下：
@@ -194,6 +195,7 @@
  
  我们把代码跑起来后，虽然调用了 methodSignatureForSelector:  方法，但程序还是崩了。难道 methodSignatureForSelector: 方法不能解决吗，我在看 methodSignatureForSelector: 方法的文档说明的时候，注意到了 forwardInvocation: 方法。
  
+ #### 2.  forwardInvocation:
  在实现 methodSignatureForSelector: 方法的同时，也必须创建 NSInvocation 对象。我理解的大概意思是，methodSignatureForSelector:  和 forwardInvocation:  必须一起实现，因为实现了 forwardInvocation:  方法，会去创建 NSInvocation 对象，并且将 NSInvocation 对象作为参数传到 forwardInvocation:  方法。
  
  那么，我们实现  forwardInvocation: 方法，并重新运行。
@@ -206,9 +208,49 @@
 
  实现了 forwardInvocation: 方法后，果然不崩了，并且还打印了 methodSignatureForSelector: 和 forwardInvocation:。
  
+ 那为什么实现了 forwardInvocation: 方法之后，不用做任何的处理，程序都不会崩溃呢。下面是我翻译官方文档对  forwardInvocation:  的说明。
  
+ 当一个对象收到一条没有相应方法的消息时，运行时系统会给接收者一个机会将消息委托给另一个接收者。它通过创建一个表示消息的 NSInvocation 对象并向接收者发送一个 forwardInvocation: 消息来委托消息，该消息包含这个 NSInvocation 对象作为参数。然后，接收者的 forwardInvocation: 方法可以选择将消息转发到另一个对象。 （如果该对象也无法响应消息，它也将有机会转发它。）
  
+ forwardInvocation: 消息因此允许一个对象与其他对象建立关系，对于某些消息，这些对象将代表它行事。从某种意义上说，转发对象能够“继承”将消息转发到的对象的某些特征。
  
+ 要响应您的对象本身无法识别的方法，除了 forwardInvocation: 之外，您还必须覆盖 methodSignatureForSelector:。转发消息的机制使用从 methodSignatureForSelector: 获得的信息来创建要转发的 NSInvocation 对象。您的覆盖方法必须为给定的选择器提供适当的方法签名，通过预先制定一个方法或通过向另一个对象询问一个方法。
+ 
+ forwardInvocation: 方法的实现有两个任务：
+ 1. 定位可以响应 anInvocation 中编码的消息的对象。该对象不必对所有消息都相同。
+ 2. 使用调用将消息发送到该对象。anInvocation 将保存结果，运行时系统将提取该结果并将其传递给原始发送者。
+ 
+ 那么，什么意思呢，我们来看一段 forwardInvocation: 的简单实现。
+ ```swift
+ - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+     if (aSelector == @selector(run)) {
+         NSLog(@"%s",__func__);
+         NSMethodSignature *signature = [NSMethodSignature signatureWithObjCTypes:"v@:"];
+         return signature;
+     }
+     return [super methodSignatureForSelector:aSelector];
+ }
+
+ - (void)forwardInvocation:(NSInvocation *)anInvocation {
+     NSLog(@"%s",__func__);
+     
+     SEL aSelector = [anInvocation selector];
+     SHAnimal *forward = [SHAnimal alloc];
+     
+     if ([forward respondsToSelector:aSelector]) {
+         [anInvocation invokeWithTarget:forward];
+     }else {
+         [super forwardInvocation:anInvocation];
+     }
+ }
+ ```
+ /Users/fatbrother/Fangss/Development/iOS/objc824/代码/03-消息传递流程/06-消息传递：消息转发-01/06-消息传递：消息转发-01/forwardInvocation实现打印.png
+ 
+ 通过这段代码和打印，正好就是印证了官方文档的注释。forwardInvocation: 实现之后，可以在方法中通过 NSInvocation 对象进行最后的消息转发处理。
+ 
+ NSInvocation 相当于事物，你只需要告诉它，是否要进行消息转发，需要的话，就像上面的例子。不需要进行转发的话，NSInvocation 对象会很乖，什么也不管，但是不会导致程序崩溃，因为只要实现了 methodSignatureForSelector: ，返回方法签名，并且创建 NSInvocation 对象，就不会崩溃。
+ 
+ forwardInvocation: 会帮我们创建一个 NSInvocation 对象，并且把这个对象传给我们，让我们通过 NSInvocation 对象进行最后的消息转发。
  
  */
 
@@ -246,6 +288,15 @@ extern void instrumentObjcMessageSends(BOOL flag);
 
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
     NSLog(@"%s",__func__);
+    
+    SEL aSelector = [anInvocation selector];
+    SHAnimal *forward = [SHAnimal alloc];
+    
+    if ([forward respondsToSelector:aSelector]) {
+        [anInvocation invokeWithTarget:forward];
+    }else {
+        [super forwardInvocation:anInvocation];
+    }
 }
 @end
 
